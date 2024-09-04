0734464da3f0e662e27e8c76b90399fccf1abf36
karolsobon <karolsobon@aol.com>
2024-09-04 22:00:36 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
+const mongoUrl = process.env.MONGODB_URL;
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Konfiguracja połączenia z MongoDB
-mongoose.connect('mongodb+srv://karolsobon:H81XqLCvlFosIOK5@cluster0.violt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
